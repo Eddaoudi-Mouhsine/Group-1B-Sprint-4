d4f0851a86589e6d85dad57184cc97833693091b
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2023-01-04 00:20:56 +0000

    Update master.blade.php

--- Realisation/Back-end/resources/views/layouts/master.blade.php
+++ Realisation/Back-end/resources/views/layouts/master.blade.php
@@ -14,7 +14,8 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+          
+            <img class="animation__shake" src=" {{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
         </div>
 
         <!-- Navbar -->
@@ -1505,7 +1506,7 @@
     <!-- ./wrapper -->
 
     @include('layouts.footer-scripts')
-    
+
 </body>
 
 </html>
