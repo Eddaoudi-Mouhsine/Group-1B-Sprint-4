45d5e0a316aa3f8e3dc9b2a634e1bf199d3771fc
Amine Salmi <92025603+AmineSalmi01@users.noreply.github.com>
2023-01-06 16:58:24 +0100

    .

--- back_end/resources/views/welcome.blade.php
+++ back_end/resources/views/welcome.blade.php
@@ -1,1 +1,21 @@
 @extends('layouts/master')
+
+@section('css')
+
+@endsection
+
+@section('title-1')
+    home
+@endsection
+
+@section('title-2')
+    dashboard
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('scripts')
+
+@endsection
